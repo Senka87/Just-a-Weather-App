b1e9b30c77c91c7f5604eef39be37e6e6b20162e
Senka87 <sunsunny969@gmail.com>
2024-11-04 22:00:45 +0100

    Added apiKey and urlKey

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "4oc0e0e6734a3f943a3ffetf95af4b14";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
